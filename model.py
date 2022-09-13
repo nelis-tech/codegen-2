7610b7e6755ff3fab032fd4cfda6f67526b673f1
nelis-tech <niels-hoekstra@hotmail.com>
2022-09-13 23:39:43 +0200

    commit updat gpt j codegen

--- model.py
+++ model.py
@@ -5,6 +5,6 @@ import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 def load_model():
-    model = AutoModelForCausalLM.from_pretrained("Salesforce/codegen-16B-multi")
-    tokenizer = AutoTokenizer.from_pretrained("Salesforce/codegen-16B-multi")
+    model = AutoModelForCausalLM.from_pretrained("moyix/codegen-16B-multi-gptj", low_cpu_mem_usage=True)
+    tokenizer = AutoTokenizer.from_pretrained("Salesforce/codegen-16B-multi") and AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
     return model, tokenizer
