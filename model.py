46252865859d2cfcc584225d9d2151753ae487f5
nelis-tech <85699478+nelis-tech@users.noreply.github.com>
2022-09-14 08:56:34 +0200

    Update model.py
    cuda update

--- model.py
+++ model.py
@@ -7,5 +7,6 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 def load_model():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = AutoModelForCausalLM.from_pretrained("moyix/codegen-16B-multi-gptj", low_cpu_mem_usage=True)
+    model.to(device)
     tokenizer = AutoTokenizer.from_pretrained("Salesforce/codegen-16B-multi") and AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
-    return device, model, tokenizer
+    return model, tokenizer, device
