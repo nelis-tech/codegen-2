2d736b11e782adf275b3c981c26881da6e1be00c
nelis-tech <85699478+nelis-tech@users.noreply.github.com>
2022-09-14 09:33:00 +0200

    Update model.py
    low_cpu_mem_usage=True deleted

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 def load_model():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = AutoModelForCausalLM.from_pretrained("moyix/codegen-16B-multi-gptj", low_cpu_mem_usage=True)
+    model = AutoModelForCausalLM.from_pretrained("moyix/codegen-16B-multi-gptj")
     model.to(device)
     tokenizer = AutoTokenizer.from_pretrained("Salesforce/codegen-16B-multi") and AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
     return model, tokenizer, device
